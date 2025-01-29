7c07e2ddccd68c3e1bfda69beb8ff62a9777244e
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-01-29 14:25:46 +0300

    KS-1-2 выполнил второе задание и надеюсь что правильно оформил название ветки + префикс проекта

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main() {
+
+    var countOfOrders: Int = 75
+    val appreciationText: String = "Благодарим за покупку"
+
+    println(countOfOrders)
+    println(appreciationText)
+
+    var countOfWorkers: Int = 2000
+
+    //println(countOfWorkers)
+
+    countOfWorkers = 1999
+
+    println(countOfWorkers)
+
+}
