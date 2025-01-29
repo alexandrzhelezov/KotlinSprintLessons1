525ed043a3a96872ba2deb061b94ba5a092555a9
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-01-29 23:33:13 +0300

    KS-1-3 выполнил третье здание (исправил числа на строки) + изменил название ветки и префикс проекта

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -3,13 +3,13 @@ package org.example.lesson_1
 fun main() {
 
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
     println("$year\n$hour\n$minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("$hour:$minute")
 
