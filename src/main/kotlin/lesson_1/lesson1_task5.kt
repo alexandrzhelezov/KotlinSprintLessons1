708ad85c41d1ae6620b87fbf4c26d7ac8729ea9e
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-02-01 11:34:47 +0300

    KS-1-5 выполнил исправления в 5-ом задании первого урока - объявил магические числа 60 и 3600 как константы и задал формат для вывода времени так, чтобы они всегда отображались с двумя цифрами

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,17 @@
 package org.example.lesson_1
 
+const val ONE_MINUTE = 60 // 1 минута в секундах
+const val SECONDS_IN_HOUR = 3600 // кол-во секунд в часе
+
 fun main() {
 
     val totalSeconds = 6480
-    val hours = "0" + totalSeconds / 3600 // получаем кол-во часов
-    val remainingSeconds = totalSeconds % 3600 // получаем остаток секунд после вычитания часа
-    val minutes = remainingSeconds / 60 // получаем минуты, поделив остаток секунд на 60
-    val seconds = "0" + remainingSeconds % 60 //получаем секунды в остатке деления остатка секунд на 60
+    val hours = totalSeconds / SECONDS_IN_HOUR // получаем кол-во часов
+    val remainingSeconds = totalSeconds % SECONDS_IN_HOUR // получаем остаток секунд после вычитания часа
+    val minutes = remainingSeconds / ONE_MINUTE // получаем минуты, поделив остаток секунд на 60
+    val seconds = remainingSeconds % ONE_MINUTE //получаем секунды в остатке деления остатка секунд на 60
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds) // формат вывода с двумя цифрами
 
-    println("$hours:$minutes:$seconds")
+    println(formattedTime)
 
 }
