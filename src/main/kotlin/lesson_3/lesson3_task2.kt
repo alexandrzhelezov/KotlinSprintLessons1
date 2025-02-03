0ccf4c88c749f44f08b9905af6966e3fea920c81
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-02-04 00:02:23 +0300

    KS-3-2 выполнил второе задание 3-го урока

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var lastName = "Андреева"
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var age = "20 лет"
+    println("$lastName $firstName $middleName, $age")
+
+    lastName = "Сидорова"
+    age = "22 года"
+    println("$lastName $firstName $middleName, $age")
+
+}
