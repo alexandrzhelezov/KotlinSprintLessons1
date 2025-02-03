44adfd65d09c714fb0ae41630ffad3900b0fc493
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-02-03 23:37:53 +0300

    KS-3-1 выполнил первое задание 3-го урока

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var welcomeMessage: String
+    val userName = "Александр"
+    welcomeMessage = "Добрый день, $userName!"
+    println(welcomeMessage)
+
+    welcomeMessage = "Добрый вечер, $userName!"
+    println(welcomeMessage)
+
+}
