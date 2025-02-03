2a925ba66acbd0ec35ba5a2b445f7ca24095cca1
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-02-03 23:14:54 +0300

    KS-1-5 исправил первое задание - убрал лишнюю константу + комментарии (коменты оставлял чисто для себя, чтобы не запутаться)

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,16 +1,15 @@
 package org.example.lesson_1
 
-const val ONE_MINUTE = 60 // 1 минута в секундах
-const val SECONDS_IN_HOUR = 3600 // кол-во секунд в часе
+const val ONE_MINUTE = 60
 
 fun main() {
 
     val totalSeconds = 6480
-    val hours = totalSeconds / SECONDS_IN_HOUR // получаем кол-во часов
-    val remainingSeconds = totalSeconds % SECONDS_IN_HOUR // получаем остаток секунд после вычитания часа
-    val minutes = remainingSeconds / ONE_MINUTE // получаем минуты, поделив остаток секунд на 60
-    val seconds = remainingSeconds % ONE_MINUTE //получаем секунды в остатке деления остатка секунд на 60
-    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds) // формат вывода с двумя цифрами
+    val hours = totalSeconds / (ONE_MINUTE * 60)
+    val remainingSeconds = totalSeconds % (ONE_MINUTE * 60)
+    val minutes = remainingSeconds / ONE_MINUTE
+    val seconds = remainingSeconds % ONE_MINUTE
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
     println(formattedTime)
 
