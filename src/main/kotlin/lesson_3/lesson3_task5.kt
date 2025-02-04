4ce17dfa78b8d6dcc2d19afeefa6849eebc0d37e
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-02-04 21:04:04 +0300

    KS-3-5 добавил в .split ещё 1 параметр для разделения строки

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,12 +3,10 @@ package org.example.lesson_3
 fun main() {
 
     val startData = "D2-D4;0"
-    val parts = startData.split(";")
-    val partsOfSteps = parts[0].split("-")
-
-    val fromWhere = partsOfSteps[0]
-    val toWhere = partsOfSteps[1]
-    val stepCount = parts[1]
+    val parts = startData.split("-", ";")
+    val fromWhere = parts[0]
+    val toWhere = parts[1]
+    val stepCount = parts[2]
 
     println(fromWhere)
     println(toWhere)
