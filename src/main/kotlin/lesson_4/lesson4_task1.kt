f722d4ed51b7e1915e0ecedff4374fb5cfae8414
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-02-05 10:35:34 +0300

    KS-4-1 исправил нейминг у константы на UPPER_SNAKE_CASE

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_4
 
-const val totalTables = 13
+const val TOTAL_TABLES = 13
 
 fun main() {
 
     val todayTables = 13
     val tomorrowTables = 9
 
-    println("Доступность столиков на сегодня: ${todayTables < totalTables}")
-    println("Доступность столиков на завтра: ${tomorrowTables < totalTables}")
+    println("Доступность столиков на сегодня: ${todayTables < TOTAL_TABLES}")
+    println("Доступность столиков на завтра: ${tomorrowTables < TOTAL_TABLES}")
 
 }
