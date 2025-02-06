b1a9ad2bf594054e0eded51a4d09e2a85de2d4d1
alexandrzhelezov <alexandrzhelezov@yandex.ru>
2025-02-06 11:25:51 +0300

    KS-4-5 заменил магические числа константами

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,5 +1,9 @@
 package org.example.lesson_4
 
+const val MIN_SHIP_MATES = 55
+const val MAX_SHIP_MATES = 70
+const val TOTAL_BOX_COUNT = 50
+
 fun main() {
 
     println("Корпус повреждён?")
@@ -14,8 +18,8 @@ fun main() {
     println("Метеоусловия благоприятны?")
     val isGoodWeather = readln().toBoolean()
 
-    val decision = (!isShipDamaged && shipTeam in 55..70 && boxCount > 50) ||
-            (isShipDamaged && shipTeam == 70 && isGoodWeather && boxCount >= 50)
+    val decision = (!isShipDamaged && shipTeam in MIN_SHIP_MATES..MAX_SHIP_MATES && boxCount > TOTAL_BOX_COUNT) ||
+            (isShipDamaged && shipTeam == MAX_SHIP_MATES && isGoodWeather && boxCount >= TOTAL_BOX_COUNT)
 
     println("Корабль может отправиться в плавание? $decision")
 
